308117e5bb9ae9919141f957a8311b10a5c63d34
Nicolas Costa <nicolascosta.ec@gmail.com>
2026-01-30 20:28:43 -0300

    Retângulo

---
+++ JAVA/exercicios/ex03.java
@@ -0,0 +1,11 @@
+package exercicios;
+
+public class ex03 {
+    public static void main(String[] args) {
+        double comprimento = 5;
+        double altura = 7;
+
+        System.out.println("A área do retângulo é de: " + comprimento * altura);
+
+    }
+}
