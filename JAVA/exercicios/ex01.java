055ef7e7a47cc6cc4a621a3949eecf99188f4e6d
Nicolas Costa <nicolascosta.ec@gmail.com>
2026-01-30 20:26:12 -0300

    Exercicio de soma

---
+++ JAVA/exercicios/ex01.java
@@ -0,0 +1,12 @@
+package exercicios;
+
+public class ex01 {
+    
+    public static void main(String[] args) {
+        int valor1 = 5;
+        int valor2 = 12;
+
+        System.out.println("A soma de "+ valor1 + " + " + valor2 +" é de: " + (valor1 + valor2));
+    };
+
+};
