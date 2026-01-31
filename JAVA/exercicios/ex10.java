7099605bd408ad10b14b90a7b0e4f4f598fba5cc
Nicolas Costa <nicolascosta.ec@gmail.com>
2026-01-30 21:55:13 -0300

    Casas decimeis em java

---
+++ JAVA/exercicios/ex10.java
@@ -0,0 +1,11 @@
+package exercicios;
+
+public class ex10 {
+    public static void main(String[] args) {
+    
+        double pi = Math.PI;
+
+        System.out.printf("%.2f", pi);
+    
+    }
+}
