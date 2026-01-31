cab6d55551c60014f6611d0e889418ebffadffa5
Nicolas Costa <nicolascosta.ec@gmail.com>
2026-01-30 22:07:23 -0300

    Quadrado

---
+++ JAVA/exercicios/ex12.java
@@ -0,0 +1,19 @@
+package exercicios;
+
+public class ex12 {
+
+    public static void imprimirQuadrado(int n){
+        n = n * n;
+    }
+
+    public static void main(String[] args) {
+        
+        int num = 5;
+        
+        imprimirQuadrado(num);
+
+        System.out.println(num);
+
+    }
+    
+}
