4af5e74b4702e6697327392708f6b5e2113c6ed5
Nicolas Costa <nicolascosta.ec@gmail.com>
2026-01-30 22:13:31 -0300

    1

--- JAVA/exercicios/ex12.java
+++ JAVA/exercicios/ex12.java
@@ -4,6 +4,7 @@ public class ex12 {
 
     public static void imprimirQuadrado(int n){
         n = n * n;
+        System.out.println(n);
     }
 
     public static void main(String[] args) {
@@ -11,8 +12,6 @@ public class ex12 {
         int num = 5;
         
         imprimirQuadrado(num);
-
-        System.out.println(num);
 
     }
     
