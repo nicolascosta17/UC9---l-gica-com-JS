e958983fc49a0fce15a32983ec205b2c736a3ce3
Nicolas Costa <nicolascosta.ec@gmail.com>
2026-01-30 21:10:16 -0300

    ex04 variável temporária

---
+++ JAVA/exercicios/ex04.java
@@ -0,0 +1,11 @@
+package exercicios;
+
+public class ex04 {
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 3;
+        int resultado = num1 + num2;
+        System.out.println(resultado);
+    }
+    
+}
