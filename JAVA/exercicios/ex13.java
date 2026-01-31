bd6a95ff0385b3beac5ee4690180ab4418dd15a4
Nicolas Costa <nicolascosta.ec@gmail.com>
2026-01-30 22:16:11 -0300

    Soma

---
+++ JAVA/exercicios/ex13.java
@@ -0,0 +1,17 @@
+package exercicios;
+
+public class ex13 {
+
+    public static void imprimirSoma(double a, double b){
+        double soma = a + b;
+        System.out.println(soma);
+
+    }
+    public static void main(String[] args) {
+
+        double num1 = 5;
+        double num2 = 4;
+
+        imprimirSoma(num1, num2);
+    }   
+}
