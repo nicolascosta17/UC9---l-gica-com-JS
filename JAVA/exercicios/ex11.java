4188976e85292909c1420bf1ec2df500d70a766e
Nicolas Costa <nicolascosta.ec@gmail.com>
2026-01-30 22:00:15 -0300

    Linhas de hifen

---
+++ JAVA/exercicios/ex11.java
@@ -0,0 +1,15 @@
+package exercicios;
+
+public class ex11 {
+
+    public static void imprimirLinha(){
+        for( int i = 0; i < 20; i++){
+            System.out.println(i);
+        }
+}
+
+    public static void main(String[] args) {
+        System.out.println("Linhas de hifen");
+        imprimirLinha();
+    }
+}
