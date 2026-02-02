80ebd5748b88e3152788ac929fbbbc25514d26a1
Nicolas Costa <nicolascosta.ec@gmail.com>
2026-02-02 19:25:57 -0300

    Revisão basica

--- JAVA/aula4/Revisao.java
+++ JAVA/aula4/Revisao.java
@@ -1,5 +1,24 @@
 package aula4;
 
-public class Revisao {
+public class Revisao { //public ==> Revisao.java
+
+    void metodoCriar(){
+
+        System.out.println("um texto");
     
+    }
+
+    public static void main(String[] args){
+        //declarações
+        int x; // Variável do tipo primitivo int
+        String palavra; // Objeto da classe String
+        double y; // Variável do tipo primitivo double
+        Double w; // Objeto da classe Double
+        
+        metodoCriar();
+
+        palavra.trim(); // Se tiver espaço antes ou depois da palavra ele corta.
+        palavra.length(); // Conta a quantidade de caracteres;
+        
+    }
 }
