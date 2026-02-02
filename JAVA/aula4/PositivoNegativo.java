efb5588c5de17d5ab5ffe655260f1001c28db146
Nicolas Costa <nicolascosta.ec@gmail.com>
2026-02-02 20:02:33 -0300

    Programa positivo e negativo

---
+++ JAVA/aula4/PositivoNegativo.java
@@ -0,0 +1,21 @@
+package aula4;
+
+import java.util.Scanner;
+
+public class PositivoNegativo {
+    
+    public static void main(String[] args){
+        Scanner entradaUser = new Scanner(System.in);
+
+        System.out.println("Entre com um número: ");
+        double num = entradaUser.nextDouble();
+
+        if (num < 0){
+            System.out.println("Este número é negativo.");
+        } else{
+            System.out.println("Este número é positivo");
+        }
+
+    }
+
+}
