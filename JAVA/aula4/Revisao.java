8db301af05bdf488540f84fdd403313f83809466
Nicolas Costa <nicolascosta.ec@gmail.com>
2026-02-02 19:36:25 -0300

    Comentario operadores relacionais

--- JAVA/aula4/Revisao.java
+++ JAVA/aula4/Revisao.java
@@ -21,4 +21,29 @@ public class Revisao { //public ==> Revisao.java
         palavra.length(); // Conta a quantidade de caracteres;
         
     }
+
+
+    /*
+    Controle de Fluxo Lógica:
+
+    Operadores Relacionais:
+
+    Igualdade: x = y (x é igual a y)
+    Diferente: x != (x é diferente de y)
+    Maior que: x > y (x é maior que y)
+    Menor que: x < y (x é menor que y)
+    Maior ou Igual que: x >= y (x pode ser maior ou igual a y)
+    Menor ou Igual que: x <= y (x pode ser menor ou igual a y)
+
+    */
+
+    void metodoIfElse(){
+        // ==========IF's Else's Switch===========
+        int x;
+        int y;
+        if (x == y){
+            System.out.println("x é maior que y");
+        }
+    }
+
 }
