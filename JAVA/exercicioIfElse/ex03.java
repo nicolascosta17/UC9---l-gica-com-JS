623b16d4401fd7e0cb2b89a030fdbb0ec07215e4
Nicolas Costa <nicolascosta.ec@gmail.com>
2026-02-02 21:18:41 -0300

    Maior ou menor ex03

---
+++ JAVA/exercicioIfElse/ex03.java
@@ -0,0 +1,25 @@
+package exercicioIfElse;
+
+import java.util.Scanner;
+
+public class ex03 {
+    
+    public static void main(String[] args) {
+        Scanner entradaUser = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+        double num1 = entradaUser.nextDouble();
+        System.out.println("Digite o segundo número: ");
+        double num2 = entradaUser.nextDouble();
+
+        if(num1 > num2){
+            System.out.printf("%s é maior que %s", num1, num2);
+        } else if(num1 < num2){
+            System.out.printf("%s é maior que %s", num2, num1);
+        }else{
+            System.out.printf("%s e %s são iguais", num1, num2);
+        }
+
+    }
+
+}
