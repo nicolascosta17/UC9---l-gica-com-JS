9b75874e1575174859daa70b94b64deb1ed6891e
Nicolas Costa <nicolascosta.ec@gmail.com>
2026-02-09 21:57:16 -0300

    Exercicio Contagem Regressiva

---
+++ JAVA/exerciciosForChar/contagemRegressiva.java
@@ -0,0 +1,13 @@
+// Contagem Regressiva: Imprima números de 10 até 1 usando while
+
+package exerciciosForChar;
+
+public class contagemRegressiva {
+    public static void main(String[] args){
+        int i = 10;
+
+        while (i >= 1) {
+            System.out.println(i--);
+        }
+    }
+}
