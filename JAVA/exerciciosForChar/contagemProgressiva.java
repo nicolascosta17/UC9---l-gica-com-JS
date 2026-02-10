61cdae08ed13f5f48518bb341a72666a090a98d2
Nicolas Costa <nicolascosta.ec@gmail.com>
2026-02-09 21:57:25 -0300

    Exercicio Contagem Progressiva

---
+++ JAVA/exerciciosForChar/contagemProgressiva.java
@@ -0,0 +1,11 @@
+// Contagem Progressiva: Imprima números de 1 até 20 usando for
+
+package exerciciosForChar;
+
+public class contagemProgressiva {
+    public static void main(String[] args){
+        for(int i = 1;i <= 20; i++ ) {
+            System.out.println(i);
+        }
+    }
+}
