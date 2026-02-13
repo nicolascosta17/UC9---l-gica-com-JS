472bd08eaacf7a4ebc66e8e4c1dabb748625c1d0
nicolascosta17 <nicolascosta.ec@gmail.com>
2026-02-13 20:26:31 -0300

    Aula String

--- JAVA/aula6/Strings.java
+++ JAVA/aula6/Strings.java
@@ -1,13 +1,43 @@
 package aula6;
 
 public class Strings{
+
     public static void main(String[] args){
-        String fruta = "bAnana";
-        char letra; // variárvel tipo char. método charAt: char(caracter) At(posição)
+        String fruta = "banAna";
+        // char letra; // variárvel tipo char. método charAt: char(caracter) At(posição)
+      
+        char letra = 'a';
         
-        for(char c = 65; c <= 122; c++){
-            System.out.printf("%d ", (char)c);
+        for(int i = 0; i < 32; i++){
+            System.out.print((char)(letra + i));
         }
+
+        // String s1 = "Alan Turing";
+        // String s2 = "Ada Lovelace";
+        // char letra;
+
+        // int diff = s1.compareTo(s2);
+
+        // if(diff < 0){
+        //     System.out.println("s1 vem antes do s2");
+        //     System.out.println(diff);
+        // }else if(diff > 0){
+        //     System.out.println("s2 vem antes do s1");
+        //     System.out.println(diff);
+        // }else{
+        //     System.out.println("s1 e s2 são iguais");
+        //     System.out.println(diff);
+        // }
+
+
+        // System.out.println(fruta.substring(0,4)); // Imprima da "0" caracter da posicão até o "4" caracter da posição: resultado = banA
+        // System.out.println(fruta.toLowerCase());
+        // System.out.println(fruta.toUpperCase());
+
+
+        // for(char c = 65; c <= 122; c++){
+        //     System.out.printf("%d ", (char)c); // Cast: só funciona com tipos primitivos exeto tipo boolean
+        // }
     
     
         // for(int i = 0; i <= fruta.length(); i++){
